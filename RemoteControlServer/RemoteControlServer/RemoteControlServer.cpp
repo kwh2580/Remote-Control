2d22b047c165d3a061ef6bf1d5f0745f80112dd2
KWH2580 <3240476304@qq.com>
2026-01-28 23:27:53 +0800

    基本功能完成

--- RemoteControlServer/RemoteControlServer/RemoteControlServer.cpp
+++ RemoteControlServer/RemoteControlServer/RemoteControlServer.cpp
@@ -18,18 +18,7 @@ int main() {
    // std::cout << CServerFun::GetFileList("C:\\Users\\32404\\Desktop\\aaa") << std::endl;
     //handle::HandleGetFileList(0, CPack());
    // CServerFun::CaptureScreenToJPG("min_full_screen.bmp",0,0,0,0,75);
-    std::thread fullScreenThread(CServerFun::RunFullScreenWindow);
-    fullScreenThread.detach();
 
-    // 模拟运行一段时间后关闭窗口（示例）
-    std::cout << "5秒后自动关闭全屏窗口..." << std::endl;
-    Sleep(5000); // 等待5秒
-    CServerFun::CloseFullScreenWindow(); // 调用关闭函数
-
-    return 0;
-    std::vector<BYTE> jpgData;
-    CServerFun::CaptureScreenToJPGMemory(jpgData, 0, 0, 0, 0, 30);
-    return 0;
     try {
         auto& server = CServer::getInstance();
         server.start(PORT);
